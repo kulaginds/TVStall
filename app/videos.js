8e4f0e8029e2718f8c0af1936f29211b693f5990
Dmitry Kulagin <kulaginds@gmail.com>
2016-11-16 19:18:27 +0300

    updated content

--- app/videos.js
+++ app/videos.js
@@ -3,23 +3,8 @@
 
     window.App.videos = [
         {
-            title: 'Elephants Dream',
-            url: 'https://archive.org/download/ElephantsDream_628/ElephantsDream_1080p_DivXPlus_512kb.mp4',
-            type: 'vod'
-        },
-        {
-            title: 'Sintel',
-            url: 'https://archive.org/download/Sintel/sintel-2048-stereo.mp4',
-            type: 'vod'
-        },
-        {
-            title: 'Big Buck Bunny',
-            url: 'https://archive.org/download/BigBuckBunnyFULLHD60FPS/Big%20Buck%20Bunny%20-%20FULL%20HD%2060FPS.mp4',
-            type: 'vod'
-        },
-        {
-            title: 'Cosmos Laundromat: First Cycle',
-            url: 'https://archive.org/download/CosmosLaundromatFirstCycle/Cosmos%20Laundromat%20-%20First%20Cycle%20(1080p).mp4',
+            title: 'Запах Женщины',
+            url: 'https://scorpio3.ru/sandbox/0E60842FE72CF7F6DA338DA5CE5DB4BB/videos/zapah_cutted.mp4',
             type: 'vod'
         }
     ];
--- shop/js/tvstall-snippet.js
+++ shop/js/tvstall-snippet.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-	var tvstall_init = false;
-	window.addAdv = function (video_id) {
+	var tvstall_video_id = null;
+		tvstall_init = false,
+		tvstall_timer = null,
+		checked_video = false;
+
+	function showAdv(video_id) {
 		if (!tvstall_init) {
+			tvstall_init = true;
+			
 			$("<div />", {
 				class: "tvstall-adv-container",
 				style: "position: absolute; left: 0; top: 0; width: 1920px; padding: 5px; border: 1px solid #FF0066; background: #FF6699"
@@ -12,10 +18,23 @@ $(document).ready(function() {
 						.append(
 							$("<a />")
 								.attr("href", "../shop/")
-								.html("Купите Apple iPhone уже сегодня!")
+								.html("Уроки танго для начинающих")
 						)
 				)
-				.insertAfter("#" + video_id);
+				.insertAfter(video_id);
 		}
+	}
+
+	function checkVideoTime() {
+		if (this.currentTime >= 89) {
+			showAdv(tvstall_video_id);
+			$(tvstall_video_id).unbind('timeupdate', checkVideoTime);
+		}
+	}
+
+	window.addAdv = function (video_id) {
+		console.log('addAdv', video_id);
+		tvstall_video_id = "#" + video_id;
+		$(tvstall_video_id).bind('timeupdate', checkVideoTime);
 	};
 });
